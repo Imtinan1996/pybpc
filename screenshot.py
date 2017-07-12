2dc78de85a350bfc316b69d70cf7f783aece0d8b
Imtinan1996 <imtinan@windowslive.com>
2017-07-12 07:47:47 +0500

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -2,6 +2,50 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 
+import cv2
+import numpy as np
+import win32gui, win32ui, win32con, win32api
+
+from mss import mss
+
+def mssShot():
+    with mss() as sct:
+        return sct.shot()
+
+def ScreenGrabber(region=(0,50,800,650)):
+
+    hwin = win32gui.GetDesktopWindow()
+
+    if region:
+            left,top,x2,y2 = region
+            width = x2 - left + 1
+            height = y2 - top + 1
+    else:
+        width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+        height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
+        left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+        top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+
+    hwindc = win32gui.GetWindowDC(hwin)
+    srcdc = win32ui.CreateDCFromHandle(hwindc)
+    memdc = srcdc.CreateCompatibleDC()
+    bmp = win32ui.CreateBitmap()
+    bmp.CreateCompatibleBitmap(srcdc, width, height)
+    memdc.SelectObject(bmp)
+    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
+    
+    signedIntsArray = bmp.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (height,width,4)
+
+    srcdc.DeleteDC()
+    memdc.DeleteDC()
+    win32gui.ReleaseDC(hwin, hwindc)
+    win32gui.DeleteObject(bmp.GetHandle())
+
+    return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+
+
 def GrabScreen(size=(0,50,800,650)):
     screen =  np.array(ImageGrab.grab(bbox=size))
     return cv2.cvtColor(screen, cv2.COLOR_BGR2RGB)
